008783f8ccd7dcefe3bbc3bdbad0a84fd33173fc
Bonnel.cunag <92771448+RonnelCunag@users.noreply.github.com>
2024-02-14 14:11:02 +0800

    Update Navigation.jsx
    add comments

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -3,24 +3,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./AuthContext"; // Adjust the path accordingly
 import '../Auth.css';
-
+/**
+ * Navigation component.
+ */
 const Navigation = () => {
+  // Use the useAuth hook to get isLoggedIn and handleLogout functions
   const { isLoggedIn, handleLogout } = useAuth();
 
   return (
+     // Navigation container
     <nav className="my-navigation">
       <img src="/assets/images/companyLogo.png" alt="Logo" />
+       {/* Menu items */}
       <ul className="menu hide">
+          {/* Home link */}
         <li>
           <a href="/dashboard">Home</a>
         </li>
+          {/* About us link */}
         <li>
           <a href="/about">About us</a>
         </li>
+        {/* External link to the company's website */}
         <li>
           <a href="https://www.tsukiden.com.ph">Contact us</a>
         </li>
       </ul>
+       {/* Testing section */}
       <div className="testing">
         {isLoggedIn ? (
           <>
