b0724a3983fb68b23e89d5f3ac1d69c6c448f540
chrstnglmtc <christinegulmatico@gmail.com>
2024-02-15 09:44:50 +0800

    fixed errors in recent merge

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from './AuthContext';
-import "../Auth.css"
+import "../Auth.css";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
 
@@ -79,95 +79,6 @@ const Landing = () => {
     
     
   );
-    <div>
-      <Navigation isLoggedIn={isLoggedIn} handleLogout={handleLogout} /> 
-     <div className="content-container">
-     <div className="text-container">
-       <div>
-         <h1
-           style={{
-             fontFamily: "Inter, sans-serif",
-             fontSize: "2rem",
-             fontWeight: 800,
-             marginBottom: "1rem",
-           }}
-         >
-           ONLINE LEARNING
-         </h1>
-         <h2
-           style={{
-             fontSize: "1.5rem",
-             fontWeight: 600,
-             marginBottom: "1rem",
-           }}
-         >
-           Gain knowledge. Learn your way. Be the best
-         </h2>
-       </div>
-       <div className="TeamA-card-container">
-         <div className="TeamA-card">
-           <div className="TeamA-card-title">
-             <h3 >SQL QUERY</h3>
-           </div>
-
-           <div className="TeamA-card-body">
-             <Link to="/">
-               <button className="teamA_button">View </button>
-             </Link>
-           </div>
-
-           <div className="TeamA-card-button">
-             <Link to="/register">
-               <button className="teamA_button">Enroll </button>
-             </Link>
-           </div>
-         </div>
-         <div className="TeamA-card">
-           <div className="TeamA-card-title">
-             <h3 >VERSION CONTROL: SVN</h3>
-           </div>
-
-           <div className="TeamA-card-body">
-             <Link to="/">
-               <button className="teamA_button">View </button>
-             </Link>
-           </div>
-
-           <div className="TeamA-card-button">
-             <Link to="/register">
-               <button className="teamA_button">Enroll </button>
-             </Link>
-           </div>
-         </div>
-         <div className="TeamA-card">
-           <div className="TeamA-card-title">
-             <h3 >HTML PROGRAMMING</h3>
-           </div>
-
-           <div className="TeamA-card-body">
-             <Link to="/">
-               <button className="teamA_button">View </button>
-             </Link>
-           </div>
-
-           <div className="TeamA-card-button">
-             <Link to="/register">
-               <button className="teamA_button">Enroll </button>
-             </Link>
-           </div>
-         </div>
-       </div>
-     </div>
-     <div className="image-container">
-       <img
-         src="/assets/images/logo.png"
-         alt="Description of the photo"
-       />
-     </div>
-   </div>
-   <Footer />
- </div>
-);
 };
 
 export default Landing;
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,69 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./AuthContext"; // Adjust the path accordingly
 import '../Auth.css';
-
 /**
  * Navigation component.
  */
-
-import Login from "./Login";
-import Register from "./Register";
-
-const CustomModal = ({ show, handleClose, children }) => {
-  const modalStyles = {
-    overlay: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      background: 'rgba(0, 0, 0, 0.2)',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backdropFilter: show ? 'blur(5px)' : 'none', 
-    },
-    modalContent: {
-      position: 'relative',
-      width: '100%',
-      maxWidth: '600px',
-      margin: '0 auto',
-      background: 'none',  // Set the background to none or transparent
-    },
-    innerContent: {
-      background: 'none',
-      padding: '20px',
-      borderRadius: '8px',
-    },
-  };
-
-  return (
-    <>
-      {show && (
-        <div style={modalStyles.overlay} onClick={handleClose}>
-          <div style={modalStyles.modalContent} onClick={(e) => e.stopPropagation()}>
-            <div style={modalStyles.innerContent}>
-              {children}
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
 const Navigation = () => {
   // Use the useAuth hook to get isLoggedIn and handleLogout functions
   const { isLoggedIn, handleLogout } = useAuth();
-  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
-  const [registerModalIsOpen, setRegisterModalIsOpen] = useState(false);
-
-  const openLoginModal = () => setLoginModalIsOpen(true);
-  const closeLoginModal = () => setLoginModalIsOpen(false);
-
-  const openRegisterModal = () => setRegisterModalIsOpen(true);
-  const closeRegisterModal = () => setRegisterModalIsOpen(false);
 
   return (
      // Navigation container
@@ -86,81 +31,26 @@ const Navigation = () => {
       </ul>
        {/* Testing section */}
       <div className="testing">
-        {/* Conditional rendering based on login status */}
         {isLoggedIn ? (
-        // If user is logged in
           <>
-             {/* Link to the user's profile */}
           <Link to="/profile">
             <button id="profile">Profile</button>
           </Link>
-             {/* Logout button */}
           <button id="logout" onClick={handleLogout}>Logout</button>
         </>
-    <div className="home-header">
-      <div className="left-container">
-        <div className="logo-container">
-          <img
-            src="/assets/images/companyLogo.png"
-           alt="Logo"
-            className="logo"
-          />
-        </div>
-        {isLoggedIn && (
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">
-                  <h3>Catalog</h3>
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <h3>Activities</h3>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        )}
-      </div>
-      <div className="right-container">
-        {!isLoggedIn && (
-          <>
-            <button className="TeamA-button" onClick={openRegisterModal}>
-              Register
-            </button>
-            <CustomModal show={registerModalIsOpen} handleClose={closeRegisterModal}>
-              <Register />
-            </CustomModal>
-          </>
-        )}
-        {isLoggedIn ? (
-          <button className="TeamA-button" onClick={handleLogout}>
-            Logout
-          </button>
         ) : (
-      // If user is not logged in
           <>
-              {/* Link to the registration page */}
             <Link to="/register">
               <button id="register">Register</button>
             </Link>
-                 {/* Link to the login page */}
             <Link to="/login">
               <button id="login">Log In</button>
             </Link>
-            <button className="TeamA-button" onClick={openLoginModal}>
-              Login
-            </button>
-            <CustomModal show={loginModalIsOpen} handleClose={closeLoginModal}>
-              <Login />
-            </CustomModal>
           </>
         )}
       </div>
-    </div>
+    </nav>
   );
 };
 
 export default Navigation;
-
